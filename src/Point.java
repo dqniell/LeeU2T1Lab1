b96c939eae1eee753ddd7b95eaf3d40d1be47fed
adamelsyaed06 <adame68@nycstudents.net>
2022-09-30 12:29:40 -0400

    first

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+
+    //instance variables
+    private int x;
+    private int y;
+
+    //constructor that accepts both instance variables
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //constructor that accepts one value and sets both x and y to the same value
+    public Point(int anyNum) {
+        x = anyNum;
+        y = anyNum;
+    }
+
+    //constructor that has no parameters and sets x and y to 0
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    //getter methods
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    //setter methods
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    //method coordinate
+    public String coordinate() {
+        return "( " + x + ", " + y + ")";
+    }
+
+    //method quadrant
 }
--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class RectangleRunner {
+import org.w3c.dom.css.Rect;
+
+public class RectangleRunner {
+    public static void main(String[] args) {
+
+        Rectangle plot1 = new Rectangle(150, 200);
+        Rectangle plot2 = new Rectangle(125);
+        Rectangle plot3 = new Rectangle();
+
+
+        plot1.setWidth(75);
+        plot2.setWidth(75);
+        plot3.setWidth(75);
+        plot2.setLength(75);
+
+        int plot1Area = plot1.calculateArea();
+        int plot2Area = plot2.calculateArea();
+        int plot3Area = plot3.calculateArea();
+
+        int totalArea = plot1Area + plot2Area + plot3Area;
+
+        System.out.println("These three plots require " + totalArea + " square feet of seed");
+    }
 }
